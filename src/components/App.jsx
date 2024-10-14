4d6c51ebf1a3fca1dac8bf812ed1935676ff1887
Kirill <123367766+KirillChern0101@users.noreply.github.com>
2024-10-14 19:52:43 +0300

    Add new

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,15 @@
+import Profile from './profile/Profile';
+import user from './profile/user.json';
+
+import { Statistics } from './statistics/Statistics';
+import data from './statistics/data.json';
+
+import { FriendList } from './friendlist/FriendList';
+import friends from './friendlist/friends.json';
+
+import { TransactionHistory } from './transaction/Transaction';
+import transactions from './transaction/transactions.json';
+
 export const App = () => {
   return (
     <div
@@ -7,10 +19,19 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
-      React homework template
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </div>
   );
 };
